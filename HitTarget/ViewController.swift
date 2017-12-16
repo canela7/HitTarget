643e4c008503d2e484559a5ce2525d55a0bd8572
canela7 <briancanela@Brians-MBP.socal.rr.com>
2017-12-15 16:48:36 -0800

    Connect IBOutlets
    Connected the IBoutlet slider

--- HitTarget/ViewController.swift
+++ HitTarget/ViewController.swift
@@ -10,29 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        currentValue = lroundf(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
 
     
     @IBAction func showAlert() {
-      let alert = UIAlertController(title: "Hello, World", message: "This is a warm up app", preferredStyle: .alert)
+        let message = "The value of the slide is: \(currentValue)"
         
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
+    
         let action = UIAlertAction(title: "Great", style: .default, handler: nil)
-        
+    
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
-        
     }
     
+
     
     
     
