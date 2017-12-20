77201a9f367d0e8642b8b9384ab08d22778106a2
canela7 <briancanela@Brians-MBP.socal.rr.com>
2017-12-20 12:53:46 -0800

    Adding extra screens
    New screens

---
+++ HitTarget/AboutMeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutMeViewController.swift
+//  HitTarget
+//
+//  Created by Brian Canela on 12/20/17.
+//  Copyright © 2017 Brian Canela. All rights reserved.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+ 
+}
---
+++ HitTarget/AboutViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutViewController.swift
+//  HitTarget
+//
+//  Created by Brian Canela on 12/20/17.
+//  Copyright © 2017 Brian Canela. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    
+    
+    
+
+}
